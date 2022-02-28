7f5d7f4047a782ece21f2469c1b1eb54cf4f48cb
ArthThapa <arththapa7@gmail.com>
2022-02-28 20:17:39 +0530

    Under dev

---
+++ components/UnderDev.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UnderDev = () => {
+  return (
+    <div className="underDev text-center">
+      <div className="text-7xl font-semibold">UNDER DEVELOPMENT ...</div>
+      <div className="text-lg">
+        Hold on while Arth and Bhumica are developing a beautiful website
+      </div>
+    </div>
+  );
+};
+
+export default UnderDev;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,37 +2,42 @@ import type { NextPage } from "next";
 import Timer from "../components/Timer";
 import Socials from "../components/Socials";
 import Image from "next/image";
+import UnderDev from "../components/UnderDev";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen">
-      <h1 className="text-black text-center text-xs best">
-        I Believe I Can Fly
-      </h1>
-      <div className="flex flex-col items-center justify-center text-center py-16 mx-4 my-12">
-        <div className="">
-          <h1 className="text-6xl font-semibold py-2 hero-t text-primary block">
-            COMING SOON ...
-          </h1>
-          <p className="best font-light text-3xl py-2 flex items-center">
-            The Best of Mine is yet to Shine.{" "}
-            <Image src={"/vids/hourglass.gif"} height={70} width={70} />
-          </p>
-        </div>
-        <div className="font-bold py-12 flex flex-col justify-center items-center">
-          <h2 className="py-1 text-5xl heading text-blue-900 mb-8 flex flex-col">
-            <div className="flex items-center text-5xl heading">
-              Launching On My Birthday{" "}
-            </div>
-            <span className="text-xs mt-[-2px] block text-black italic">
-              ~ programmed by my brother, designed by me
-            </span>
-          </h2>
-          <Timer />
-        </div>
-        <Socials />
-      </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <UnderDev />
     </div>
+    // // <div className="min-h-screen">
+    //   {/* <h1 className="text-black text-center text-xs best">
+    //     I Believe I Can Fly
+    //   </h1>
+    //   <div className="flex flex-col items-center justify-center text-center py-16 mx-4 my-12">
+    //     <div className="">
+    //       <h1 className="text-6xl font-semibold py-2 hero-t text-primary block">
+    //         COMING SOON ...
+    //       </h1>
+    //       <p className="best font-light text-3xl py-2 flex items-center">
+    //         The Best of Mine is yet to Shine.{" "}
+    //         <Image src={"/vids/hourglass.gif"} height={70} width={70} />
+    //       </p>
+    //     </div>
+    //     <div className="font-bold py-12 flex flex-col justify-center items-center">
+    //       <h2 className="py-1 text-5xl heading text-blue-900 mb-8 flex flex-col">
+    //         <div className="flex items-center text-5xl heading">
+    //           Launching On My Birthday{" "}
+    //         </div>
+    //         <span className="text-xs mt-[-2px] block text-black italic">
+    //           ~ programmed by my brother, designed by me
+    //         </span>
+    //       </h2>
+    //       <Timer />
+    //     </div>
+    //     <Socials />
+    //   </div> */}
+
+    // // </div>
   );
 };
 
